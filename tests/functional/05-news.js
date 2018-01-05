af5f82361b988bd21844ff515d0c4c5770eb79f6
Courtney Myers <courtney.myers@erg.com>
2018-01-05 15:06:52 -0500

    Add functional tests for each neop template page and move page scrolling behavior into seperate file, which is included in each functional test file

---
+++ tests/functional/05-news.js
@@ -0,0 +1,13 @@
+const { describe, it, before } = intern.getInterface('bdd');
+const scrollPage = require('./includes/scrollPage.js');
+
+describe('News', () => {
+  before(async ({ remote }) => {
+    await remote.get('https://neoceanplanning.com/news/november-15-16-rpb-meeting-briefing-packet-available/');
+    await remote.sleep(2000);
+  });
+
+  it('scrolling...', async ({ remote }) => {
+    await scrollPage(remote);
+  });
+});
